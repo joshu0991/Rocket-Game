82059edf575af77027ab5f3207638ddc8c6561a0
Josh <joshualilly@me.com>
2014-05-19 19:48:06 -0700

    started the binary tree

---
+++ Address_Book/Bin_Node.h
@@ -0,0 +1,21 @@
+/*
+ * Bin_Node.h
+ *
+ *  Created on: May 19, 2014
+ *      Author: josh
+ */
+
+#ifndef BIN_NODE_H_
+#define BIN_NODE_H_
+
+struct node
+{
+	node* left;
+	node* right;
+	int value;
+
+};
+
+
+
+#endif /* BIN_NODE_H_ */
---
+++ Address_Book/Bin_Tree.h
@@ -0,0 +1,18 @@
+/*
+ * Bin_Tree.h
+ *
+ *  Created on: May 19, 2014
+ *      Author: josh
+ */
+
+#ifndef BIN_TREE_H_
+#define BIN_TREE_H_
+
+class Bin_Tree
+{
+public:
+
+
+};
+
+#endif /* BIN_TREE_H_ */
